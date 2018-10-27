9beba1dcc1965dec942c71362de279bbf47e155d
Nick Burrin <NickBurrin@utexas.edu>
2018-10-26 20:57:10 -0700

    Friday work

--- Conventions/Operators overloading/src/Task.kt
+++ Conventions/Operators overloading/src/Task.kt
@@ -3,13 +3,17 @@ import TimeInterval.*
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
 enum class TimeInterval { DAY, WEEK, YEAR }
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = this.addTimeIntervals(timeInterval, 1)
+operator fun MyDate.plus(timeIntervals: RepeatedTimeInterval): MyDate
+        = this.addTimeIntervals(timeIntervals.timeInterval, timeIntervals.number)
+operator fun TimeInterval.times(num: Int) = RepeatedTimeInterval(this, num)
 
 fun task1(today: MyDate): MyDate {
     return today + YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
